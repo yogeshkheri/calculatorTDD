eb40e0be3319bf1cb8a688862337cd9a8156b402
Yogeshvce <yogesh@highperflow.com>
2024-10-29 14:05:56 +0530

    Calculator changes to accept n values with two delimiters

--- src/main/java/com/proj/Calculator.java
+++ src/main/java/com/proj/Calculator.java
@@ -8,8 +8,8 @@ public class Calculator {
     public static int add(String numbers){
         if(numbers.isBlank()){
             return  0;
-        }else if (numbers.contains(",")) { // condition for any number values like 1,2,3
-            List<String> testList = Arrays.asList(numbers.split(","));
+        }else if (numbers.contains(",")) { // condition for any number values with two delimiters like 1/n2,5
+            List<String> testList = Arrays.asList(numbers.split("[,\n" + "]"));
             return testList.stream().mapToInt(Integer::valueOf).sum();
         } else { // condition for other values
             return convertToInt(numbers);
