27a3974f7f4e1e49520985005dbc19263094a856
Yogeshvce <yogesh@highperflow.com>
2024-10-29 14:03:12 +0530

    Calculator changes to accept n values with comma

--- src/main/java/com/proj/Calculator.java
+++ src/main/java/com/proj/Calculator.java
@@ -1,13 +1,16 @@
 package com.proj;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
 
     public static int add(String numbers){
         if(numbers.isBlank()){
             return  0;
-        }else if (numbers.contains(",")) { // condition for two values like 1,4
-            String[] splitText = numbers.split(",");
-            return convertToInt(splitText[0]) + convertToInt(splitText[1]);
+        }else if (numbers.contains(",")) { // condition for any number values like 1,2,3
+            List<String> testList = Arrays.asList(numbers.split(","));
+            return testList.stream().mapToInt(Integer::valueOf).sum();
         } else { // condition for other values
             return convertToInt(numbers);
         }
